0a395778d1167faf92e2dd923ba32e68bb69e4a1
haiderimam07 <haiderimam1207@gmail.com>
2026-05-17 16:05:46 +0530

    setting up pipeline

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import multer from 'multer'
+import { Queue } from 'bullmq'
+
+const queue = new Queue('file-upload-queue',{
+  connection:{
+            host:'localhost',
+            port:'6379',
+        },
+})
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -16,12 +24,18 @@ const upload = multer({ storage: storage })
 
 
 const app= express()
-app.use(cors())
+app.use(cors());
+
 
 app.get('/',(req,res)=>{
     return res.json({status: 'All Good'})
 });
-app.post('/upload/pdf', upload.single('pdf') , (req, res)=>{
+app.post('/upload/pdf', upload.single('pdf') , async (req, res)=>{
+  await queue.add('file-ready', JSON.stringify({
+    filename:req.file.originalname,
+    destination:req.file.destination,
+    path:req.file.path
+  }))
     res.json({message: 'uploaded'})
 })
 
