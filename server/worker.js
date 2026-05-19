a9cf7111b331c843079bcc8e0474726625601003
haiderimam07 <haiderimam1207@gmail.com>
2026-05-19 14:01:48 +0530

    done chinking

--- server/worker.js
+++ server/worker.js
@@ -1,6 +1,28 @@
 import { Worker } from "bullmq";
+import {OpenAIEmbeddings } from "@langchain/OpenAi"
+import {QdrantVectorStore} from "@langchain/qdrant"
+import {Document} from "@langchain/core/documents"
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf"
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+
 const worker= new Worker('file-upload-queue', async (job) =>{
     console.log(`job:`, job.data);
+    const data= JSON.parse(job.data)
+    // steps to do now as pdf has reached to the server
+    /*
+        Read the data from the path that we can get form data.path 
+        chunk the pdf
+        call the open AI embedding model for the chunk of the pdf
+        store the chunk in quadrant db
+        load the pdf using langchian pdf loader
+    */
+    // load the pdf     
+    const loader = new PDFLoader(data.path)
+    const docs = await loader.load()
+    const splitter = new RecursiveCharacterTextSplitter({ chunkSize: 300, chunkOverlap: 0 })
+    const texts = await splitter.splitDocuments(docs)
+    console.log(texts)
+
     },
     {
         concurrency:100,
